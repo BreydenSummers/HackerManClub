fd61aaa697bf2089564e7ceb01ce82601fca0486
Arick Smith <ArickTheDisciple@gmail.com>
2023-07-15 17:04:21 -0600

    Good refreasher on how this works

---
+++ NotsoClever/script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById("canvas1");
+const contxt = canvas.getContext('2d');
+console.log(contxt)
+
+const CANVAS_WIDTH = canvas.width;
+const CANVAS_HEIGHT = canvas.height;
