657be39e970d432db0068c3ea8975a0750bd3875
Arick Smith <ArickTheDisciple@gmail.com>
2023-07-15 18:35:29 -0600

    Made the end of the animations :)

--- NotsoClever/script.js
+++ NotsoClever/script.js
@@ -1,3 +1,9 @@
+let playerState = 'idle';
+const dropdown = document.getElementById('animations');
+dropdown.addEventListener('change', function(e){
+    playerState = e.target.value;
+});
+
 const canvas = document.getElementById("canvas1");
 const contxt = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 600;
@@ -8,8 +14,7 @@ const playerImage = new Image();
 playerImage.src = './images/shadow_dog.png';
 const spriteW = 575;
 const spriteH = 523;
-let frameX = 0;
-let frameY = 4;
+
 let gameFrame = 0;
 const staggerFrames = 10;
 const spriteAnimations = [];
@@ -21,7 +26,40 @@ const animationStates = [
     {
         name: 'jump',
         frames: 7,
+    },
+    {
+        name: 'fall',
+        frames: 7,
+    },
+    {
+        name: 'run',
+        frames: 9,
+    },
+    {
+        name: 'stun',
+        frames: 10,
+    },
+    {
+        name: 'sit',
+        frames: 5,
+    },
+    {
+        name: 'roll',
+        frames: 7,
+    },
+    {
+        name: 'bite',
+        frames: 7,
+    },
+    {
+        name: 'die',
+        frames: 12,
+    },
+    {
+        name: 'hit',
+        frames: 4,
     }
+
 
 ];
 animationStates.forEach((state, index) => {
@@ -40,10 +78,11 @@ animationStates.forEach((state, index) => {
 function animate(){
     //Built in fuction of canvas context give top left corner then bottom right
     contxt.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    let postition = Math.floor(gameFrame/staggerFrames) % spriteAnimations["idle"].location.length;
-    frameX = spriteW * postition;
+    let postition = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].location.length;
+    let frameX = spriteW * postition;
+    let frameY = spriteAnimations[playerState].location[postition].y;
     //Image you want to draw, src x then src y coords, src width and src height. destin x and y and w and h
-    contxt.drawImage(playerImage, frameX, frameY * spriteH, spriteW, spriteH, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    contxt.drawImage(playerImage, frameX, frameY, spriteW, spriteH, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
     gameFrame++;
     requestAnimationFrame(animate);
